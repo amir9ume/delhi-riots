b15b28ebaeae65e3936e617e827f36d361db8784
Hamza Afridi <hamzaafridi@gmail.com>
2020-04-10 19:25:11 +0100

    renamed the file for better organization, Deleted unnecessary package imports

--- scripts/get_all_users.py
+++ scripts/get_all_users.py
@@ -1,16 +1,8 @@
-import warnings
-warnings.simplefilter('ignore')
 from multiprocessing import Pool
 import twint #twitter scrapping tool
 import nest_asyncio #twint has dependency on this
-import os #to measure file size
-import time #to process time data
 import pandas as pd
-import seaborn as sns #plotting tool
-import matplotlib.pyplot as plt #plotting tool
-import threading #multiprocessing
 import asyncio
-from time import sleep
 import os
 
 nest_asyncio.apply() #so that multiple requests can be made at the same time
@@ -23,7 +15,7 @@ def search_query(args):
     user_client = twint.Config()
     user_client.Store_csv = True
     user_client.Hide_output = True
-    user_client.Output = os.path.join("tmp", "users_parallel_"+str(i)+".csv")
+    user_client.Output = os.path.join("data/tmp", "users_parallel_"+str(i)+".csv")
 
     for j, user in enumerate(usernames):
         print(i, ">>>>>", j)
@@ -33,11 +25,7 @@ def search_query(args):
     print(i, "finished")
 
 if __name__=="__main__":
-    participant_handles_df = pd.read_csv('tweets.csv', usecols=['username'])
-
-    participant_handles_df.drop_duplicates(inplace=True)
-    participant_handles_df.to_csv("unique_users.csv") # store users for future use
-
+    participant_handles_df = pd.read_csv('data/unique_users.csv', usecols=['username'])
     print("number of unique users: ",participant_handles_df.shape[0])
 
     NUM_THREAD = 10
